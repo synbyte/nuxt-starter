4092639084c80b632a792dee8c12c8a68e106839
synbyte <rashnikov2.0@gmail.com>
2024-04-03 20:41:33 +0000

    learning

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,26 @@
+<script setup>
+const links = [
+  {
+    label: 'Home',
+    to: '/'
+  },
+  {
+    label: 'About',
+    to: '/about'
+  }
+]
+</script>
+
+
 <template>
-<div>
-  <nav>
-    navbar
-    <NuxtLink to="/">Home</NuxtLink>
-    <NuxtLink to="/about">About</NuxtLink>
-  </nav>
+<div class='flex flex-col'>
+  
+  <UHorizontalNavigation class="border-b border-gray-400" :links="links"/>
+  <UContainer>
+  <div class='p-10 bg-red-900'>
   <slot />
+  </div>
+  </UContainer>
   
 </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div>
     <h1>Index page</h1>
     <p>This is the index page</p>
-    <NuxtLink to="/about">About</NuxtLink>
+    <USkeleton class='h-32'></USkeleton>
+    
   </div>
 </template>
